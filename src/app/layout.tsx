58d203aa7832d24a55dba4108b4436b16ce25866
Joshua Miller <joshuae.miller100@gmail.com>
2024-01-31 02:45:04 +0100

    Properly installed airbnb style guide

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,11 @@
-import "./globals.css";
+import { ReactNode } from 'react';
+import './globals.css';
 
-export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
-	return (
-		<html lang="en">
-			<body>{children}</body>
-		</html>
-	);
+type RootLayoutProps = Readonly<{ children: ReactNode }>;
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
